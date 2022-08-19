d1f263b9e3445c5b1bf1bef8027f310b00b5f3ae
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-20 00:24:02 +0530

    add demo data for implement project

---
+++ src/component/data/data.js
@@ -0,0 +1,26 @@
+export const questions = [
+  {
+    id: 1,
+    title: "React Js",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.” The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout",
+  },
+  {
+    id: 2,
+    title: "HTML",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.” The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout",
+  },
+  {
+    id: 3,
+    title: "Python",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.” The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout",
+  },
+  {
+    id: 4,
+    title: "Java",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.” The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout",
+  },
+];
